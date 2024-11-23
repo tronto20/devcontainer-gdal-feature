50d37bed75b706c868ef16dae338d4f7e28cfc24
tronto20 <tronto980@gmail.com>
2024-11-23 22:23:23 +0900

    test: add test

--- src/java-binding/install.sh
+++ src/java-binding/install.sh
@@ -12,12 +12,12 @@ echo "Build Multi CPU: $CPU"
 apt-get update
 apt-get install -y build-essential wget cmake libtiff-dev libgeotiff-dev libproj-dev python3-dev python3-pip libxml2-dev libgeos-dev libjson-c-dev swig liblerc-dev ant proj-bin python3-numpy
 
-mkdir /tmpbuild
+mkdir -p /tmp/gdal
 
 # ref : https://gdal.org/development/building_from_source.html
-wget -O /tmpbuild/gdal.tar.gz https://download.osgeo.org/gdal/${GDALVERSION}/gdal-${GDALVERSION}.tar.gz
-tar -xzvf /tmpbuild/gdal.tar.gz -C /tmpbuild/
-cd /tmpbuild/gdal-${GDALVERSION}
+wget -O /tmp/gdal/gdal.tar.gz https://download.osgeo.org/gdal/${GDALVERSION}/gdal-${GDALVERSION}.tar.gz
+tar -xzvf /tmp/gdal/gdal.tar.gz -C /tmp/gdal/
+cd /tmp/gdal/gdal-${GDALVERSION}
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
@@ -25,8 +25,11 @@ cmake --build . -j ${CPU:-4}
 cmake --build . --target install
 ldconfig
 
-#pip install /tmpbuild/gdal-${GDALVERSION}/build/swig/python
-cp /tmpbuild/gdal-${GDALVERSION}/build/swig/java/libgdalalljni.so /usr/local/lib/ && \
-    cp /tmpbuild/gdal-${GDALVERSION}/build/swig/java/libgdalalljni.so /usr/lib/jni/
+#pip install /tmp/gdal/gdal-${GDALVERSION}/build/swig/python
+mkdir -p /usr/local/lib
+mkdir -p /usr/lib/jni
 
-rm -r /tmp
+cp /tmp/gdal/gdal-${GDALVERSION}/build/swig/java/libgdalalljni.so /usr/local/lib/
+cp /tmp/gdal/gdal-${GDALVERSION}/build/swig/java/libgdalalljni.so /usr/lib/jni/
+
+rm -r /tmp/gdal
